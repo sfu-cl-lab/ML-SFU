f1475636e838b8f1b5b02553ebe9a175d4769fad
rmar3a <rkm3@sfu.ca>
2022-03-05 18:04:51 -0800

    Updated repository to build and deploy using GitHub Actions.
    - Created a Bash script to build and deploy the webpage to GitHub Pages.
    - Added a YAML file to use GitHub Actions instead of Travis CI.
    - Delete the Travis CI file since it is no longer needed.
    - The latest version of the Python YAML library seems to be incompatible with
      how it is currently used in parse_content.py, so the call has been updated to
      use safe_load instead of load.

---
+++ deployment/update-gh-pages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+repoURI="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remoteName="origin"
+mainBranch="master"
+targetBranch="gh-pages"
+buildDirectory="dist"
+
+cd "$GITHUB_WORKSPACE"
+
+# Commit and push the changes to GitHub Pages.
+git config user.name "$GitHub Actions"
+git config user.email "github.actions@bots.github.com"
+
+git checkout "$targetBranch"
+
+echo ml.cs.sfu.ca > $buildDirectory/CNAME
+git add "$buildDirectory"
+
+git commit -m "Updated GitHub Pages."
+if [ $? -ne 0 ]
+then
+  echo "Nothing to commit!"
+  exit 0
+fi
+
+git remote set-url "$remoteName" "$repoURI"
+git push --force-with-lease "$remoteName" "$targetBranch"
