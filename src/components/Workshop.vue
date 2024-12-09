3326c74eda8f4d0d3f3dbadd39049045b8f77f65
Angel Chang <angelx@gmail.com>
2024-12-09 10:23:37 -0800

    Update NeurIPS 2024 news item

--- src/components/Workshop.vue
+++ src/components/Workshop.vue
@@ -3,9 +3,11 @@
     <h3><a :href="workshop.url">{{workshop.title }}</a></h3>
       <div><b>{{getDayOfWeek(workshop.date)}} {{workshop.date}}</b></div>
       <div v-if="workshop.location"><b>{{workshop.location}}</b></div>
-      <div v-if="workshop.advisor"><b>Advisor:</b> {{workshop.advisor}}</div>
-      <div v-if="workshop.organizer"><b>Organizer:</b> {{workshop.organizer}}</div>
-      <div v-if="workshop.speaker"><b>Speaker:</b> {{workshop.speaker}}</div>
+      <div v-if="workshop.roles">
+        <div v-for="(role,index) in workshop.roles" :key="'r-' + index">
+          <b>{{role.title | uppercase }}:</b> {{role.name}}
+        </div>
+      </div>
   </article>
 </template>
 
@@ -20,6 +22,12 @@ export default {
     getDayOfWeek: function(dateStr) {
       var date = new Date(dateStr + 'T12:00:00.000-07:00')
       return date.toLocaleDateString('en-US', { weekday: 'long' })
+    }
+  },
+  filters: {
+    uppercase: function (value) {
+      if (!value) return ''
+      return value.charAt(0).toUpperCase() + value.slice(1)
     }
   },
   props: ['workshop']
